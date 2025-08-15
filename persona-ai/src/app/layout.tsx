d64049f2625687bf22687c8eb7c880c5317b220b
Rahul Aggarwal <91338578+Rahulagg13@users.noreply.github.com>
2025-08-15 15:20:25 +0530

    add analytics

--- persona-ai/src/app/layout.tsx
+++ persona-ai/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/provider";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <Toaster richColors position="top-center" />
+          <Analytics />
         </Providers>
       </body>
     </html>
